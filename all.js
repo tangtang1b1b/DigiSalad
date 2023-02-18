57654742cfb91e85fed2e74476a51d32bf217877
tangtang1b1b <a0981632653@gmail.com>
2023-02-19 02:18:08 +0800

    two

--- all.js
+++ all.js
@@ -3,6 +3,7 @@
 const vm = Vue.createApp({
     data(){
         return{
+            check:true,
             open:false,
             imgs:['./image/asia.svg','./image/asia.svg','./image/asia.svg','./image/asia.svg'],
             cards:[{logo:"1",h3:"UX Design"},{logo:"2",h3:"UX Design"},{logo:"3",h3:"website development"},
@@ -11,31 +12,51 @@ const vm = Vue.createApp({
         }
     },
     methods: {
-      view(){
-        let head = document.querySelector(".head");
-        let hamber = document.querySelectorAll(".hamber .path");
-        let logo = document.querySelector(".logoinner");
-        window.addEventListener("scroll",()=>{
-            if(window.scrollY > 0){
-                head.style.backgroundColor="white";
-                hamber.forEach(v=>{
-                    v.style.backgroundColor="#414042";
-                });
-                this.open=true;
-                logo.style.animation="logo 0.3s both";
+        view(){
+            let head = document.querySelector(".head");
+            let hamber = document.querySelectorAll(".hamber .path");
+            let logo = document.querySelector(".logoinner");
+            window.addEventListener("scroll",()=>{
+                if(window.scrollY > 0){
+                    head.style.backgroundColor="white";
+                    hamber.forEach(v=>{
+                        v.style.backgroundColor="#414042";
+                    });
+                    this.open=true;
+                    logo.style.animation="logo 0.3s both";
 
-            }else{
-                head.style.backgroundColor="transparent";
-                hamber.forEach(v=>{
-                    v.style.backgroundColor="white";
-                });
+                }else{
+                    head.style.backgroundColor="transparent";
+                    hamber.forEach(v=>{
+                        v.style.backgroundColor="white";
+                    });
+                    setTimeout(()=>{
+                        logo.style.animation="logoback 0.3s both";
+                    },300);
+                    this.open=false;
+                }
+            });
+        },
+        hamber(){
+            let hamber = document.querySelectorAll(".hamber .path");
+            if(this.check==true){
+                hamber[2].style.width="0px";
+                hamber[0].style.top="9px";
                 setTimeout(()=>{
-                    logo.style.animation="logoback 0.3s both";
+                    hamber[0].style.transform="rotate(45deg)";
+                    hamber[1].style.transform="rotate(135deg)";
                 },300);
-                this.open=false;
+                this.check=false;
+            }else{
+                hamber[0].style.transform="rotate(0deg)";
+                hamber[1].style.transform="rotate(0deg)";
+                setTimeout(()=>{
+                    hamber[2].style.width="26px";
+                    hamber[0].style.top="0px";
+                },300);
+                this.check=true;
             }
-        });
-      },
+        },
     },
     mounted(){
         this.view();
